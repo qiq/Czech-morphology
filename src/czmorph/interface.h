25ab5c11d55fe81fee216c05f2286742a8158048
Miroslav Spousta <spousta@ufal.mff.cuni.cz>
2011-08-04 13:37:42 +0000

    Removing czmorph (why is it still there?).

--- src/czmorph/interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INTERFACE_H_
-#define _INTERFACE_H_
-
-int lemmatize_init(const char *dictionary, const char *unknown_rules, const char *tag_table);
-char *lemmatize_token(const char *token, int is_punct, int is_abbr, int is_number, int dot_follows, int hyphen_follows);
-void lemmatize_destroy();
-
-#endif
